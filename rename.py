e0610b6d706c5f4fb52086b1c340a097f281ad62
danielchang2002 <danielchang2002@gmail.com>
2022-07-21 09:52:31 -0500

    :rocket: mp3

--- rename.py
+++ rename.py
@@ -2,4 +2,4 @@ import os
 for dir in os.listdir():
     if not os.path.isdir(dir) or dir == ".git" or dir == "MetaPhlAn2":
         continue
-    os.rename(dir + f"/{dir}.txt", dir + f"/{dir}_metaphlan2.txt")
+    os.rename(dir + f"/metaphlan3.txt", dir + f"/{dir}_metaphlan3.txt")
--- run.py
+++ run.py
@@ -2,14 +2,17 @@ import os
 import subprocess
 
 for dir in os.listdir():
-    if os.path.isdir(dir) and not dir == ".git":
+    if os.path.isdir(dir) and not dir == ".git" and not dir == "MetaPhlAn2":
         os.chdir(dir)
-        forward = dir + "_1.fastq"
-        backward = dir + "_2.fastq"
-        if forward not in os.listdir():
-            os.chdir("../")
-            continue
-        print(os.listdir())
-        command = f"gmhi --fastq1 {forward} --fastq2 {backward} -o GMHI.txt"
-        subprocess.call(command.split())
+        print(dir)
+        subprocess.call(
+            ["metaphlan",
+            "QC_1P.fastq.gz,QC_2P.fastq.gz",
+            #"--bowtie2db", database_dir,
+            "--bowtie2out", "bowtieout.bowtie2.bz2",
+            "--index", "mpa_v30_CHOCOPhlAn_201901",
+            "--nproc", "16", "--input_type", "fastq",
+            "-o", "metaphlan3.txt", "--add_viruses", "--unknown_estimation"
+            ]
+        )
         os.chdir("../")
